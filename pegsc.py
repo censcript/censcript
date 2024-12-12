823fea9bbe46f6b2e189fbd0e2815665b74e0624
censcript <cenc16995@gmail.com>
2024-12-12 23:31:23 +0800

    Update pegsc.py

--- pegsc.py
+++ pegsc.py
@@ -32,5 +32,5 @@ urls = [
     "https://www.bevy.be/bevyfiles/portugal2.xml",
 ]
 
-# Memanggil fungsi untuk mendownload semua EPG
+# Memanggil fungsi untuk mendownload semua EPGs
 download_epg(urls)
