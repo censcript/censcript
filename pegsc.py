a58ea35261272d16f7bba393e8c9c09e097c5e1e
censcript <cenc16995@gmail.com>
2024-12-28 02:36:21 +0800

    Update pegsc.py

--- pegsc.py
+++ pegsc.py
@@ -34,6 +34,7 @@ urls = [
     "https://www.bevy.be/bevyfiles/italy2.xml",
     "https://www.bevy.be/bevyfiles/portugal2.xml",
     "https://astvstreamingpro.my.id/epg/alkass.php",
+    "https://astvstreamingpro.my.id/epg/vision.php",
 ]
 
 # Memanggil fungsi untuk mendownload semua EPG
