91a92cfadb95e9bb5f9753397a82e2eb65083471
censcript <cenc16995@gmail.com>
2025-08-03 23:13:26 +0800

    Update pegsc.py

--- pegsc.py
+++ pegsc.py
@@ -40,6 +40,7 @@ urls = [
     "https://manusiabiasa.serv00.net/cen/epg/skyde.php",
     "https://manusiabiasa.serv00.net/cen/epg/shahid.php",
     "https://manusiabiasa.serv00.net/cen/epg/mytv.php",
+    "https://manusiabiasa.serv00.net/cen/epg/cosmote.php",
     #"https://cindo.mra.my.id/epg/sky.php",
     #"https://cindo.mra.my.id/epg/mytv.php",
 ]
